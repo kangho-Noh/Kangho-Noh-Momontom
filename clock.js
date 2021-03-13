856aa28202f407f50efcf90b01d58fc18c2d1f99
kangho-Noh <rkdgh98@khu.ac.kr>
2021-03-13 13:25:47 +0900

    Add localStorage + input box

--- clock.js
+++ clock.js
@@ -1,11 +1,22 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector(".js-title");
+  clockTitle = clockContainer.querySelector(".js-title"),
+  clockAMPM = clockContainer.querySelector(".js-clock__ampm");
 
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
-  const hours = date.getHours();
+  let hours = date.getHours();
   const seconds = date.getSeconds();
+  let ampm = "AM";
+  if (hours == 0) {
+    hours = 12;
+  } else if (hours < 24 && hours >= 12) {
+    ampm = "PM";
+    if (hours > 12) {
+      hours -= 12;
+    }
+  }
+  clockAMPM.innerHTML = `${ampm} `;
   clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
